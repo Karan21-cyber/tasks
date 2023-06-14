22e5de9deee52f1ea5a47e65d8e59d35dd1a5e34
Karan Chaudhary <tharukaran930@gmail.com>
2023-06-14 23:17:18 +0545

    first_commit

--- Parking_System/backend/Model/paymentModel.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const paymentSchema = mongoose.Schema({
-  user: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  reservespot: [{ type: mongoose.Schema.Types.ObjectId, ref: "reservespot" }],
-  totalamount:{type:String},
-});
